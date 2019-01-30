0d821c5e1f70b0501515d39ee39f7486ec874af7
Manley, Timothy (NCWS) <manley.t@c02wm4hrj1gg.school.etoncollege.org.uk>
2019-01-30 16:53:07 +0000

    Initial Commit

---
+++ A-Level Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  A-Level Project
+//
+//  Created by Manley, Timothy (NCWS) on 30/01/2019.
+//  Copyright © 2019 Manley, Timothy (NCWS). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ A-Level ProjectTests/A_Level_ProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  A_Level_ProjectTests.swift
+//  A-Level ProjectTests
+//
+//  Created by Manley, Timothy (NCWS) on 30/01/2019.
+//  Copyright © 2019 Manley, Timothy (NCWS). All rights reserved.
+//
+
+import XCTest
+@testable import A_Level_Project
+
+class A_Level_ProjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
