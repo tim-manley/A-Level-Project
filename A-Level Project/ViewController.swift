631013e1b3cd0541730d09ecf1d085a4a8568f4c
Manley, Timothy (NCWS) <manley.t@c02wm4hrj1gg.school.etoncollege.org.uk>
2019-01-30 17:21:04 +0000

    Basic login screen layout and added outlets to ViewController

--- A-Level Project/ViewController.swift
+++ A-Level Project/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!    
 
     override func viewDidLoad() {
         super.viewDidLoad()
